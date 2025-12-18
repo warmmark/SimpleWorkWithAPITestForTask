879e8d2dcb5946baa32a587327d4e09aaa7a37d3
warmmark <zhongli.goldheart@gmail.com>
2025-12-18 22:12:40 +0300

    fix first test

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -2,8 +2,8 @@ import { fetchPost } from "../api.js";
 
 describe('tests for fetchPost from api.js', () => {
     test('test for correct behaviour if id = 1', async () => {
-        const result = await fetchPost(1);
-        expect(result).toEqual({ id: 1, title: 'First Post' });
+        const resultForFirstTest = await fetchPost(1);
+        expect(resultForFirstTest).toEqual({ id: 1, title: 'First Post' });
     })
 
     test('test for correct behaviour if id != 1', async () => {
