d3452d3637b5db2e46876f64c5c36132ae4fff5f
warmmark <zhongli.goldheart@gmail.com>
2025-12-18 22:19:38 +0300

    add third test for correct title length if id = 1 for function fetchPost with id != 1 in api.test.js

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -9,4 +9,9 @@ describe('tests for fetchPost from api.js', () => {
     test('test for correct behaviour if id != 1', async () => {
         await expect(() => fetchPost(21)).rejects.toThrow('Post not found');
     })
+
+    test('test for correct title if id = 1', async () => {
+        const resultForThirdTest = await fetchPost(1);
+        expect(resultForThirdTest.title.length).toBe(10);
+    })
 })
