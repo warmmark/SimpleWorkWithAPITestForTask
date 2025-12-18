d01ec9ecbf3d555bb3f9665f320566a422730b11
warmmark <zhongli.goldheart@gmail.com>
2025-12-18 21:22:10 +0300

    copy function fetchPost from task into api.js

---
+++ api.js
@@ -0,0 +1,11 @@
+export function fetchPost() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (id === 1) {
+                resolve({ id: 1, title: 'First Post' });
+            } else {
+                reject(new Error('Post not found'))
+            }
+        }, 1000)
+    })
+}
