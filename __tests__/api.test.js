2ef28fec6d743e8a3f8480f7ee4aab89eeceeece
warmmark <zhongli.goldheart@gmail.com>
2025-12-18 21:58:08 +0300

    add first test for correct behaviour f function fetchPost with id = 1 in api.test.js

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -1,4 +1,8 @@
 import { fetchPost } from "../api.js";
 
 describe('tests for fetchPost from api.js', () => {
+    test('test for correct behaviour if id = 1', async () => {
+        const result = await fetchPost(1);
+        expect(result).toEqual({ id: 1, title: 'First Post' });
+    })
 })
