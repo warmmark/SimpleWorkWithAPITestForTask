87327944d8210f2a55bd8a679e737d34b366182c
warmmark <zhongli.goldheart@gmail.com>
2025-12-18 22:10:35 +0300

    add second test for correct behaviour for function fetchPost with id != 1 in api.test.js

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -5,4 +5,8 @@ describe('tests for fetchPost from api.js', () => {
         const result = await fetchPost(1);
         expect(result).toEqual({ id: 1, title: 'First Post' });
     })
+
+    test('test for correct behaviour if id != 1', async () => {
+        await expect(() => fetchPost(21)).rejects.toThrow('Post not found');
+    })
 })
